f9c5bd83c4369d307ca3b514983f823afda7584c
BrettBrewster <bbrewster@sealanddesign.com>
2020-03-16 09:49:42 -0400

    Save jwt locally upon successful login

---
+++ static/js/store_jwt.js
@@ -0,0 +1,11 @@
+let vars = [], hash;
+let hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+for(var i = 0; i < hashes.length; i++)
+{
+    hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+}
+let jwt = vars['http://localhost:5000/categories#access_token'];
+console.log(jwt);
+localStorage.setItem("jwt", jwt);
